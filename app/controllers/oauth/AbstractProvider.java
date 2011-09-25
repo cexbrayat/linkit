327a51d7af018a5fdce312dd8c93f697efa50f8b
Agnes Crepet <agnes.crepet@gmail.com>
2011-09-25 09:02:08 +0200

    fix NullPointerException on fetch OAuth data (AbstractProvider)

--- app/controllers/oauth/AbstractProvider.java
+++ app/controllers/oauth/AbstractProvider.java
@@ -12,24 +12,38 @@ import play.libs.OAuth.ServiceInfo;
 public abstract class AbstractProvider implements OAuthProvider {
 
     static protected ServiceInfo getServiceInfo(String provider) {
-        final String requestTokenURL = Play.configuration.getProperty(provider+".requestTokenUrl");
-        final String accessTokenURL = Play.configuration.getProperty(provider+".accessTokenUrl");
-        final String authorizeURL = Play.configuration.getProperty(provider+".authorizeUrl");
-        final String consumerKey = Play.configuration.getProperty(provider+".consumerKey");
-        final String consumerSecret = Play.configuration.getProperty(provider+".consumerSecret");
+        final String requestTokenURL = Play.configuration.getProperty(provider + ".requestTokenUrl");
+        final String accessTokenURL = Play.configuration.getProperty(provider + ".accessTokenUrl");
+        final String authorizeURL = Play.configuration.getProperty(provider + ".authorizeUrl");
+        final String consumerKey = Play.configuration.getProperty(provider + ".consumerKey");
+        final String consumerSecret = Play.configuration.getProperty(provider + ".consumerSecret");
         return new ServiceInfo(requestTokenURL, accessTokenURL, authorizeURL, consumerKey, consumerSecret);
     }
-        
+
     static String getStringPropertyFromJson(JsonElement response, String property) {
-        return response.getAsJsonObject().get(property).getAsString();
-    }
-        
-    static String getStringPropertyFromJson(JsonObject object, String property) {
-        return object.get(property).getAsString();
-    }
-    
-    static Long getLongPropertyFromJson(JsonElement response, String property) {
-        return response.getAsJsonObject().get(property).getAsLong();
+        JsonObject jObjet = response.getAsJsonObject();
+        if (jObjet.get(property) != null) {
+            return jObjet.get(property).getAsString();
+        } else {
+            return null;
+        }
     }
 
+    static String getStringPropertyFromJson(JsonObject object, String property) {
+        if (object.get(property) != null) {
+            return object.get(property).getAsString();
+        } else {
+            return null;
+        }
+    }
+
+    static Long getLongPropertyFromJson(JsonElement response, String property) {
+        JsonObject jObjet = response.getAsJsonObject();
+        if (jObjet.get(property) != null) {
+            return jObjet.getAsLong();
+        } else {
+            return null;
+        }
+
+    }
 }
