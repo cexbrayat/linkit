02146230703062c490dcc13e0f8ccd20232bb7cb
Cyril Lacote <cyril.lacote@gmail.com>
2011-11-06 12:48:47 +0700

    Badge computers unit tests refactoring (working on one dedicated new member, not from initial data, for ensuring clean state for badge computing (no links, no comment, ...))

--- test/controllers/badge/SpeakerFriendBadgeComputerTest.java
+++ test/controllers/badge/SpeakerFriendBadgeComputerTest.java
@@ -4,51 +4,27 @@ import java.util.EnumSet;
 import java.util.List;
 import java.util.Set;
 import models.Badge;
-import models.Member;
 import models.Speaker;
-import models.Staff;
-import org.junit.After;
-import org.junit.Before;
 import org.junit.Test;
-import play.test.Fixtures;
-import play.test.UnitTest;
 
 /**
  * Unit tests for {@link SpeakerFriendBadgeComputer}
  * @author Sryl <cyril.lacote@gmail.com>
  */
-public class SpeakerFriendBadgeComputerTest extends UnitTest {
+public class SpeakerFriendBadgeComputerTest extends AbstractBadgeComputerTest {
 
-    private SpeakerFriendBadgeComputer computer = new SpeakerFriendBadgeComputer();
-
-    @Before
-    public void setUp() {
-        Fixtures.deleteAllModels();
-        Fixtures.loadModels("data.yml");
-    }
-
-    @After
-    public void tearDown() {
-        Fixtures.deleteAllModels();
-    }
-    
-    
-    @Test
-    public void notGranted() {
-        final Member m = Member.all().first();
-        final Set<Badge> actualBadges = computer.compute(m, new BadgeComputationContext());
-        assertTrue(actualBadges.isEmpty());
+    public SpeakerFriendBadgeComputerTest() {
+        super(new SpeakerFriendBadgeComputer());
     }
     
     @Test
     public void granted() {
-        final Member m = Member.all().first();
         // Member links all speaker Members
         final List<Speaker> speakers = Speaker.findAll();
         for (Speaker s : speakers) {
-            m.addLink(s);
+            member.addLink(s);
         }
-        final Set<Badge> actualBadges = computer.compute(m, new BadgeComputationContext());
+        final Set<Badge> actualBadges = computer.compute(member, new BadgeComputationContext());
         assertEquals(EnumSet.of(Badge.SpeakerFriend), actualBadges);
     }
 }
