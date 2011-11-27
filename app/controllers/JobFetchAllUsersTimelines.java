60b26805d29d97ab706faedbc911e832efef8dda
Cyril Lacote <cyril.lacote@gmail.com>
2011-11-27 12:37:42 +0800

    Fixed periodicity of JobFetchUserTimelines (I should stop committing it every time after concurrency tests....)

--- app/controllers/JobFetchAllUsersTimelines.java
+++ app/controllers/JobFetchAllUsersTimelines.java
@@ -14,7 +14,7 @@ import play.jobs.Job;
  * Asynchronous fetch of user timelines on external providers (Google+, Twitter)
  * @author Sryl <cyril.lacote@gmail.com>
  */
-@Every("30s")
+@Every("5min")
 public class JobFetchAllUsersTimelines extends Job {
 
     @Override
