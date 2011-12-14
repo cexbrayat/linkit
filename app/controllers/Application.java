4e23ff8b5fd1898049215a61e02d92d996e48b4b
cexbrayat <cexbrayat@gmail.com>
2011-12-14 22:12:33 +0100

    home page alternative

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -21,9 +21,9 @@ public class Application extends PageController {
         }
         
         // Three recent articles
-        List<Article> articles = Article.recents(1, 3);
-        List<Talk> sessions = Talk.recents(1, 3);
-        List<Member> members = Member.recents(1, 9);
+        List<Article> articles = Article.recents(1, 2);
+        List<Talk> sessions = Talk.recents(1, 2);
+        List<Member> members = Member.recents(1, 6);
         List<Map> tags = Interest.getCloud();
         render(articles, sessions, members, tags);
     }
