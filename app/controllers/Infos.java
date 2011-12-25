f93a96c4aad22e7ee8e3f61b1e614f327f4724f0
Agnes Crepet <agnes.crepet@gmail.com>
2011-12-25 17:48:58 +0100

    infos, email staff

--- app/controllers/Infos.java
+++ app/controllers/Infos.java
@@ -1,7 +1,13 @@
 package controllers;
 
-public class Infos extends PageController {
+import java.util.List;
+import models.LinkItEmail;
+import models.Member;
+import models.Staff;
+import play.Logger;
+import play.data.validation.Validation;
 
+public class Infos extends PageController {
 
     public static void orga() {
         render("Infos/orga.html");
@@ -9,6 +15,10 @@ public class Infos extends PageController {
 
     public static void faq() {
         render("Infos/faq.html");
+    }
+
+    public static void inscriptions() {
+        render("Infos/inscriptions.html");
     }
 
     public static void kit() {
@@ -25,5 +35,20 @@ public class Infos extends PageController {
 
     public static void hotels() {
         render("Infos/hotels.html");
+    }
+
+    public static void sendStaff(LinkItEmail linkitemail) {
+        Member from = Member.findByLogin(Security.connected());
+        List<Member> members = Staff.findAll();
+        linkitemail.recipients = members;
+        linkitemail.from = from;
+        validation.valid(linkitemail);
+        if (Validation.hasErrors()) {
+            Logger.error(Validation.errors().toString());
+            contacts();
+        }
+        linkitemail.send();
+        flash.success("Merci pour votre email!");
+        contacts();
     }
 }
