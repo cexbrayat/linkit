c7048a9f1693a0c27f584ba5f569a89a7b55cdab
Agnes Crepet <agnes.crepet@gmail.com>
2011-12-29 12:23:50 +0100

    ajout job asynchrone pour appel non bloquant a WeezEvent lors de la MAJ profil

---
+++ app/controllers/JobMajUserRegisteredTicketing.java
@@ -0,0 +1,28 @@
+package controllers;
+
+import helpers.ticketing.WeezEvent;
+import models.Member;
+import play.Logger;
+import play.jobs.Job;
+
+/**
+ * Asynchronous fetch to check if users are registered at the ticketing partner
+ * @author Agnes <agnes.crepet@gmail.com>
+ */
+public class JobMajUserRegisteredTicketing extends Job {
+
+    private long idMember;
+
+    public JobMajUserRegisteredTicketing(long idMember) {
+        this.idMember = idMember;
+    }
+
+    @Override
+    public void doJob() {
+        Logger.info("BEGIN JOB JobMajUserRegisteredTicketing for member with id %d", idMember);
+        Member member = Member.findById(idMember);
+        WeezEvent.updateRegisteredAttendee(member);
+        Logger.info("END JOB JobMajUserRegisteredTicketing for member %d", idMember);
+
+    }
+}
