49d4d8676eaa099c8d363eb88144c3526bd80d12
nicolas pelloux-prayer <npelloux-prayer@gmail.com>
2012-01-16 02:51:43 +0100

    Implement salt + hash on password storage for linkit security provider & signup features

--- test/SecurityTest.java
+++ test/SecurityTest.java
@@ -1,4 +1,8 @@
 import models.BaseDataUnitTest;
+import models.Member;
+import models.activity.Activity;
+import models.activity.SignUpActivity;
+import models.auth.LinkItAccount;
 import controllers.Security;
 
 import org.junit.*;
@@ -10,5 +14,22 @@ public class SecurityTest extends BaseDataUnitTest {
         assertFalse(Security.authenticate("bob", "bob"));
         assertFalse(Security.authenticate("ced", "bob"));
         assertTrue(Security.authenticate("bob", "secret"));
+    }
+    
+    @Test
+    public void signupAndConnect() {
+        
+        final String login = "signupAndConnectTest";
+        final String password = "password";
+        // Unknown user
+        Member nouveau = Member.findByLogin(login);
+        assertNull(nouveau);
+        
+        nouveau = new Member(login);
+        nouveau.preregister(new LinkItAccount(password));
+        nouveau.register();
+        
+        assertFalse(Security.authenticate(login, "foo"));
+        assertTrue(Security.authenticate(login,password));
     }
 }
