a80bb911b9448ccbbad3321667762084d1d4670f
Cyril Lacote <cyril.lacote@gmail.com>
2012-02-07 23:54:44 -0700

    Non-important activities (i.e. look activities) of member no more displayed (in profile, i n dashboard). Still displayed in session show page, but with dynamic ajax.

--- app/controllers/Sessions.java
+++ app/controllers/Sessions.java
@@ -94,8 +94,7 @@ public class Sessions extends PageController {
         if (!noCount) {
             talk.lookedBy(Member.findByLogin(Security.connected()));
         }
-        List<Activity> activities = Activity.recentsBySession(talk, 1, 5);
-        render(talk, activities);
+        render(talk);
     }
 
     public static void postComment(
