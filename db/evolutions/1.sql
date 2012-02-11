622df138bcfcd031c2773227f6608a30d336e725
Cyril Lacote <cyril.lacote@gmail.com>
2012-02-11 01:44:19 -0700

    Add DB scripts as Play! evolutions

---
+++ db/evolutions/1.sql
@@ -0,0 +1,28 @@
+# --- !Ups
+
+alter table activity add column level int(11);
+
+update activity set level=2 where DTYPE like 'Comment%';
+update activity set level=2 where DTYPE like 'Earn%';
+update activity set level=3 where DTYPE like 'Link%';
+update activity set level=4 where DTYPE like 'Look%';
+update activity set level=1 where DTYPE like 'New%';
+update activity set level=2 where DTYPE like 'NewVote%';
+update activity set level=2 where DTYPE like 'Shared%';
+update activity set level=2 where DTYPE like 'Earn%';
+update activity set level=2 where DTYPE like 'Sign%';
+update activity set level=3 where DTYPE like 'Status%';
+update activity set level=3 where DTYPE like 'UpdateProfile%';
+update activity set level=1 where DTYPE like 'UpdateSession%';
+COMMIT;
+
+alter table activity DROP column important;
+
+# --- !Downs
+
+alter table activity DROP column level;
+alter table activity add column important bit(1);
+
+update activity set important=true where DTYPE like 'Comment%';
+update activity set important=false where DTYPE like 'Look%';
+COMMIT;
