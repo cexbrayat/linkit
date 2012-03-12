cb8c0a4b30f4797734f8b2de969af6e28cc2eff7
Agnes Crepet <agnes.crepet@gmail.com>
2012-03-12 13:31:34 +0100

    maj inscription + admin weezevent

---
+++ app/controllers/Operations.java
@@ -0,0 +1,27 @@
+package controllers;
+
+import models.Role;
+import play.Play;
+import play.mvc.With;
+
+/**
+ *
+ * @author Agnes <agnes.crepet@gmail.com>
+ */
+@Check(Role.ADMIN_MEMBER)
+@With(SecureLinkIt.class)
+public class Operations extends PageController {
+
+    static final int JOBS_DELAY_AFTER_UPDATE = Integer.valueOf(Play.configuration.getProperty("linkit.job.delayAfterMemberUpdate", "2"));
+ 
+    public static void index() {
+        render();
+    }
+    
+    
+    public static void syncWeezEvent(long mailingId) {
+        new JobFetchRegisteredTicketingForAllUsers().in(JOBS_DELAY_AFTER_UPDATE);
+        index();
+    }
+   
+}
