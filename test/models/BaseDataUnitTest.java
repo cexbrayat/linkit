914ef3b9d98c6977fdf88d740924908a20f834ec
Cyril Lacôte <cyril.lacote@gmail.com>
2012-04-10 01:11:35 +0200

    Try to have better tests reloading after code modification : workaround for https://play.lighthouseapp.com/projects/57987/tickets/425-error-in-reloading-a-yml-file (should be fixed in 1.2.5), thanks to http://stackoverflow.com/questions/6266684/cannot-load-fixture-because-of-wrong-duplicate-id-detection

--- test/models/BaseDataUnitTest.java
+++ test/models/BaseDataUnitTest.java
@@ -14,7 +14,9 @@ public abstract class BaseDataUnitTest extends UnitTest {
 
     @Before
     public void setUp() {
-        Fixtures.deleteAllModels();
+        // CLA 10/04/2012 : Fix https://play.lighthouseapp.com/projects/57987/tickets/425-error-in-reloading-a-yml-file before Play! 1.2.5 release.
+        // cf. http://stackoverflow.com/questions/6266684/cannot-load-fixture-because-of-wrong-duplicate-id-detection
+        Fixtures.deleteDatabase();
         Fixtures.loadModels("data.yml");
     }
 
