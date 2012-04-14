486aa5372d73fc5536996a2506efdd50b4ad11a9
jripault <tluapir@gmail.com>
2012-04-14 20:50:48 +0200

    Ajout logo Google Modif sur la page Sponsor

--- app/models/Sponsor.java
+++ app/models/Sponsor.java
@@ -1,9 +1,14 @@
 package models;
 
 import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+
 import org.apache.commons.lang.StringUtils;
 import play.data.validation.Required;
 import play.modules.search.Indexed;
+
+import java.util.List;
 
 /**
  * A Mix-IT sponsor, giving a fucking load of money to buy better sandwiches than the ones given at WSN Paris.
@@ -12,14 +17,25 @@ import play.modules.search.Indexed;
 @Entity
 @Indexed
 public class Sponsor extends Member {
+    public enum Level{
+      GOLD, SILVER, BRONZE
+    }
 
     @Required
     public String logoUrl;
+
+    @Enumerated(EnumType.STRING)
+    public Level level;
 
     public Sponsor(String login) {
         super(login);
         // Can't call addBadge() on a transient instance (and don't want to trigger activity)
         this.badges.add(Badge.Sponsor);
+    }
+
+    public static List<Sponsor> findByLevel(Level level){
+        return Sponsor.find(
+            "select distinct m from Member m where level = ?", level).fetch();
     }
 
     @Override
