6622b502188b8cf51c3b2a7ff11f12b5438dc7f6
clacote <cyril@ninja-squad.com>
2012-12-21 09:52:08 +0100

    Data patch : member not registered (for 2013)

--- db/evolutions/4.sql
+++ db/evolutions/4.sql
@@ -5,5 +5,8 @@ ALTER TABLE Session add column event varchar(10) not null;
 update Member_Badges mb set mb.badges = 26 where mb.badges = 4;
 commit;
 
+# Member.ticketingRegistered = false
+update Member m set m.ticketingRegistered = false;
+
 # --- !Downs
 ALTER TABLE Session drop column event;
