0cea8eaccf9863e2f17ff8ef8da0894f72c9149d
clacote <cyril@ninja-squad.com>
2013-01-18 23:15:01 +0100

    New talk attributes.

--- app/models/Talk.java
+++ app/models/Talk.java
@@ -6,9 +6,7 @@ import play.data.validation.Required;
 import play.modules.search.Indexed;
 import play.mvc.Router;
 
-import javax.persistence.Entity;
-import javax.persistence.EnumType;
-import javax.persistence.Enumerated;
+import javax.persistence.*;
 import java.util.List;
 import models.activity.CommentSessionActivity;
 
@@ -20,9 +18,28 @@ import models.activity.CommentSessionActivity;
 @Indexed
 public class Talk extends Session {
 
-    @Required
     @Enumerated(EnumType.STRING)
     public Track track;
+
+    /** Bénéfices pour les participants */
+    @Lob
+    public String benefits;
+
+    @Required
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false, length = 20)
+    public TalkFormat format = TalkFormat.Talk;
+
+    public Integer maxAttendees;
+
+    @Required
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false, length = 20)
+    public TalkLevel level = TalkLevel.Experienced;
+
+    /** Markdown enabled */
+    @Lob
+    public String comment;
 
     public static List<Talk> findAllOn(ConferenceEvent event) {
         return find("event = ?", event).fetch();
