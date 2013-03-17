666d43360be927886d25375fc0317521850fcc25
clacote <cyril@ninja-squad.com>
2013-03-17 18:28:28 +0100

    JSON API : by id

--- app/controllers/api/ApiMembers.java
+++ app/controllers/api/ApiMembers.java
@@ -1,5 +1,6 @@
 package controllers.api;
 
+import com.google.gson.JsonSerializer;
 import models.*;
 import play.mvc.Controller;
 
@@ -7,22 +8,32 @@ import java.util.List;
 
 public class ApiMembers extends Controller {
 
+    private static JsonSerializer MEMBER_SERIALIZERS[] = new JsonSerializer[] {
+        new MemberJsonSerializer(), new SponsorJsonSerializer(), new StaffJsonSerializer()
+    };
+
     public static void speakers() {
         List<Member> speakers = Talk.findAllSpeakersOn(ConferenceEvent.CURRENT);
-        Controller.renderJSON(speakers, new MemberJsonSerializer());
+        renderJSON(speakers, MEMBER_SERIALIZERS);
     }
 
     public static void sponsors() {
         List<Sponsor> sponsors = Sponsor.findOn(ConferenceEvent.CURRENT);
-        Controller.renderJSON(sponsors, new SponsorJsonSerializer());
+        renderJSON(sponsors, MEMBER_SERIALIZERS);
     }
     public static void staff() {
         List<Staff> staff = Staff.findAll();
-        Controller.renderJSON(staff, new MemberJsonSerializer(), new SponsorJsonSerializer(), new StaffJsonSerializer());
+        renderJSON(staff, MEMBER_SERIALIZERS);
     }
 
     public static void members() {
         List<Member> members = Member.findAll();
-        renderJSON(members, new MemberJsonSerializer(), new SponsorJsonSerializer(), new StaffJsonSerializer());
+        renderJSON(members, MEMBER_SERIALIZERS);
+    }
+
+    public static void member(long id) {
+        Member member = Member.findById(id);
+        notFoundIfNull(member);
+        renderJSON(member, MEMBER_SERIALIZERS);
     }
 }
