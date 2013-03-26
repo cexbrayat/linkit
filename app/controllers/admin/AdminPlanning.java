fd23b44ad72d31fea8118ce902012a8c85b2fd1c
clacote <cyril@ninja-squad.com>
2013-03-26 01:40:27 +0100

    Admin planning : basic error management

--- app/controllers/admin/AdminPlanning.java
+++ app/controllers/admin/AdminPlanning.java
@@ -1,5 +1,6 @@
 package controllers.admin;
 
+import com.google.common.base.Joiner;
 import controllers.Check;
 import controllers.SecureLinkIt;
 import models.ConferenceEvent;
@@ -8,8 +9,10 @@ import models.Talk;
 import models.planning.PlanedSlot;
 import models.planning.Slot;
 import play.mvc.Controller;
+import play.mvc.Http;
 import play.mvc.With;
 
+import java.util.ArrayList;
 import java.util.EnumMap;
 import java.util.List;
 import java.util.Map;
@@ -29,15 +32,31 @@ public class AdminPlanning extends Controller {
         if (talkIds == null) badRequest();
         if (slots.size() != talkIds.size()) badRequest();
 
+        List<String> errors = new ArrayList<String>();
         Map<Slot, Talk> planning = new EnumMap<Slot, Talk>(Slot.class);
         for (int i = 0; i < slots.size(); ++i) {
             if (talkIds.get(i) >= 0) {
+
+                final Talk talk = Talk.findById(talkIds.get(i));
+                if (talk == null) {
+                    errors.add("Talk id " + talkIds.get(i) + " unknoww");
+                } else if (planning.containsValue(talk)) {
+                    errors.add("Talk " + talk + " planifié en double");
+                }
+
+                if (planning.containsKey(slots.get(i))) {
+                    errors.add("Slot " + slots.get(i) + " déjà affecté");
+                }
+
                 planning.put(slots.get(i), Talk.<Talk>findById(talkIds.get(i)));
             }
         }
 
-        PlanedSlot.save(planning);
+        if (!errors.isEmpty()) {
+            error(Http.StatusCode.BAD_REQUEST, Joiner.on(", ").join(errors));
+        }
 
+        PlanedSlot.save(planning);
         index();
     }
 }
