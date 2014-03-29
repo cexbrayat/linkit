6b29426974b14c9dc6c2c5dfc147e3c454a75bb2
mpetitdant <mathieu.petitdant@gmail.com>
2014-03-29 17:54:33 +0100

    Fixed cyril comments

--- app/models/planning/Room.java
+++ app/models/planning/Room.java
@@ -2,13 +2,13 @@ package models.planning;
 
 public enum Room {
 
-    A1("Grand Amphi"), //used by mixit14
-    A2("Petit Amphi"), //used by mixit14
     S1("Gosling"), //used by mixit13, mixit14
     S2("Eich"), //used by mixit13, mixit14
     S3("Nonaka"), //used by mixit13, mixit14
     S4("Dijkstra"), //used by mixit13, mixit14
-    S5("Turing"); //used by mixit13
+    S5("Turing"), //used by mixit13
+    A1("Grand Amphi"), //used by mixit14
+    A2("Petit Amphi"); //used by mixit14
 
     private String name;
 
