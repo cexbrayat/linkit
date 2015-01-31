eb4ecc15028b010693f348bcf40e84b2d0da95ae
mpetitdant <mathieu.petitdant@gmail.com>
2015-01-31 16:30:19 +0100

    solving clacote comments on PR #330

--- db/evolutions/30.sql
+++ db/evolutions/30.sql
@@ -1,6 +1,6 @@
 # --- !Ups
 alter table session add column guest bit default false;
-update session set guest=1 where id in (select distinct session_id
+update session set guest=true where id in (select distinct session_id
 from comment where lower(comment.content) like '%guest speaker%');
 
 # --- !Downs
